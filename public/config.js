e1d8c069149b23c6f27c3128531635f696c70984
Hannan Rozal <hannanrozal@gmail.com>
2023-06-28 18:32:41 +0500

    main

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://honey-movies.hannanrozal7695.workers.dev",
+  VITE_TMDB_READ_API_KEY: "906758f174a8895d483a49ff6b499690"
+};
